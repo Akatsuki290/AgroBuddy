6c53cb3400132de19d6f795963d6dbf74171534a
souravsingpardeshi <63045639+souravsingpardeshi@users.noreply.github.com>
2021-10-02 17:24:54 +0530

    ui update for cards

--- agrobuddy/src/component/Menu.js
+++ agrobuddy/src/component/Menu.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import "./Menu.css";
-import border from "./plantb.png"
 import Card from "./Card.js";
 function Menu() {
     return (
         <div className="Menu-main">
-           <h1>Our services</h1>
+            <h1>══════⊹⊱≼Our Services≽⊰⊹══════</h1>
                     <div className="Menu">
             <Card name="crop prediction" img="https://img.icons8.com/color/96/000000/growing-plant.png" />
             <Card name="Disease prediction" img ="https://img.icons8.com/external-justicon-lineal-color-justicon/100/000000/external-pesticide-farming-and-gardening-justicon-lineal-color-justicon.png" />
