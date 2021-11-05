71ad992df01693c4b59942ff73821665dc233718
souravsingpardeshi <63045639+souravsingpardeshi@users.noreply.github.com>
2021-11-05 16:35:21 +0530

    first module

--- agrobuddy/src/component/Menu.js
+++ agrobuddy/src/component/Menu.js
@@ -6,7 +6,7 @@ function Menu() {
         <div className="Menu-main">
             <h1>══════⊹⊱≼Our Services≽⊰⊹══════</h1>
                     <div className="Menu">
-            <Card name="crop prediction" img="https://img.icons8.com/color/96/000000/growing-plant.png" />
+            <Card name="crop prediction" img="https://img.icons8.com/color/96/000000/growing-plant.png" link="https://share.streamlit.io/souravsingpardeshi/crop_project/main/app.py" />
             <Card name="Disease prediction" img ="https://img.icons8.com/external-justicon-lineal-color-justicon/100/000000/external-pesticide-farming-and-gardening-justicon-lineal-color-justicon.png" />
             <Card name="Infopedia" img="https://img.icons8.com/fluency/96/000000/green-earth.png"/>
             <Card name="market" img="https://img.icons8.com/color/96/000000/tractor.png"/>
